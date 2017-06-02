6e996a4b53c87678c0b1d695d6efd64158f3c22f
kyuwankim <lpnids@gmail.com>
2017-06-02 15:08:57 +0900

    add transaction.addToBackStack(null);

--- app/src/main/java/com/kyuwankim/android/fragment/ListFragment.java
+++ app/src/main/java/com/kyuwankim/android/fragment/ListFragment.java
@@ -6,24 +6,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ListFragment extends Fragment {
-
+    MainActivity activity;
 
     public ListFragment() {
         // Required empty public constructor
     }
 
+    public void setActivity(MainActivity activity) {
+        this.activity = activity;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        // 프래그먼트 메인 레이아웃을 inflate 하고
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        // 안의 위젯들을 코드에 연결한다
+        Button btnGoDetail = (Button) view.findViewById(R.id.buttonGodetail);
+        btnGoDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activity.addDetail();
+            }
+        });
+
+        return view;
     }
 
 }
--- app/src/main/java/com/kyuwankim/android/fragment/MainActivity.java
+++ app/src/main/java/com/kyuwankim/android/fragment/MainActivity.java
@@ -1,13 +1,52 @@
 package com.kyuwankim.android.fragment;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    ListFragment list;
+    DetailFragment detail;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        createFragments();
+        addList();
+
+
+    }
+
+    public void addDetail() {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frame, detail);
+        // transaction 처리 전체를 stack에 담아놨다가 안드로이드의 back 버튼으로 뒤로가기를 할 수 있다
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+    public void addList() {
+
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.frame, list);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
+
+    public void createFragments() {
+        list = new ListFragment();
+        list.setActivity(this);
+        detail = new DetailFragment();
+        detail.setActivity(this);
+    }
+
+    public void backToList() {
+        super.onBackPressed();
     }
 }
